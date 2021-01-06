93e68790632a802625d8dd8dbb9418f7701bc58b
Florian Geierstanger <florian@geierstanger.org>
2021-01-06 16:44:06 +0100

    construct imageShortcode manually

--- .eleventy.js
+++ .eleventy.js
@@ -27,10 +27,39 @@ module.exports = function (eleventyConfig) {
 };
 
 
-async function imageShortcode(src, alt, sizes) {
+// async function imageShortcode(src, alt, sizes) {
+//   let metadata = await Image(src, {
+//     widths: [300, 600],
+//     formats: ["webp", "jpeg"],
+//     filenameFormat: function (id, src, width, format, options) {
+//       const extension = path.extname(src);
+//       const name = path.basename(src, extension);
+//       return `${name}-${width}w.${format}`;
+//     },
+//     urlPath: "/assets/images/",
+//     outputDir: "./site/assets/images/"    
+//   });
+
+//   let imageAttributes = {
+//     alt,
+//     sizes,
+//     loading: "lazy",
+//     decoding: "async",
+//     class: "img--responsive"
+//   };
+
+//   return Image.generateHTML(metadata, imageAttributes);
+// }
+
+async function imageShortcode(src, alt, sizes = "100vw") {
+  if(alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+
   let metadata = await Image(src, {
-    widths: [300, 600],
-    formats: ["webp", "jpeg"],
+    widths: [300],
+    formats: ['avif', 'jpeg'],
     filenameFormat: function (id, src, width, format, options) {
       const extension = path.extname(src);
       const name = path.basename(src, extension);
@@ -40,15 +69,20 @@ async function imageShortcode(src, alt, sizes) {
     outputDir: "./site/assets/images/"    
   });
 
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-    class: "img--responsive"
-  };
+  let lowsrc = metadata.jpeg[0];
 
-  return Image.generateHTML(metadata, imageAttributes);
+  return `<picture class="img--responsive">
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+    }).join("\n")}
+      <img
+        src="${lowsrc.url}"
+        width="${lowsrc.width}"
+        height="${lowsrc.height}"
+        alt="${alt}"
+        loading="lazy"
+        decoding="async">
+    </picture>`;
 }
 
 
