1b3d507f02f126eaac1f78dfdc5916cca5fd6c4a
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-04-04 22:16:06 +0200

    [FEATURE] Prevent diogonal swipes

--- src/assets/javascript/swipe.js
+++ src/assets/javascript/swipe.js
@@ -3,11 +3,15 @@ export class SwipeListener {
     this.elem = document.querySelector(rootSelector);
     this.touchstartX = 0;
     this.touchendX = 0;
+    this.touchstartY = 0;
+    this.touchendY = 0;
 
     this.elem.addEventListener('touchstart', this.handleTouchStart.bind(this));
     this.elem.addEventListener('touchend', this.handleTouchEnd.bind(this));
 
     this.handlers = {
+      swipeUp: [],
+      swipeDown: [],
       swipeLeft: [],
       swipeRight: [],
       tap: []
@@ -16,26 +20,38 @@ export class SwipeListener {
 
   handleTouchStart(evt) {
     this.touchstartX = evt.touches[0].clientX;
+    this.touchstartY = evt.touches[0].clientY;
   }
 
   handleTouchEnd(evt) {
     this.touchendX = evt.changedTouches[0].clientX;
+    this.touchendY = evt.changedTouches[0].clientY;
     this.handleSwipe();
   }
 
   handleSwipe() {
     const distX = this.touchendX - this.touchstartX;
+    const distY = this.touchendY - this.touchstartY;
     const absDistX = Math.abs(distX);
+    const absDistY = Math.abs(distY);
 
-    if (absDistX < 20) {
+    if (absDistX < 20 && absDistY < 20) {
       this.handlers.tap.forEach(fn => fn());
       return;
     }
 
-    if (distX > 0) {
-      this.handlers.swipeRight.forEach(fn => fn());
+    if (absDistX > absDistY) {
+      if (distX > 0) {
+        this.handlers.swipeRight.forEach(fn => fn());
+      } else {
+        this.handlers.swipeLeft.forEach(fn => fn());
+      }
     } else {
-      this.handlers.swipeLeft.forEach(fn => fn());
+      if (distY > 0) {
+        this.handlers.swipeDown.forEach(fn => fn());
+      } else {
+        this.handlers.swipeUp.forEach(fn => fn());
+      }
     }
   }
 
