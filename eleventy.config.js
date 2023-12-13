b0fdb10710bcb34647d2d39efc56042b46fb3472
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-12-13 18:08:21 +0100

    wip: webc component example

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,49 +1,52 @@
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const path = require("path");
 const Image = require("@11ty/eleventy-img");
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setTemplateFormats("njk,svg,woff2,md");
+	eleventyConfig.setTemplateFormats("njk,svg,woff2,md");
 
-  eleventyConfig.addPlugin(EleventyVitePlugin);
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	eleventyConfig.addPlugin(EleventyVitePlugin);
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	eleventyConfig.addPlugin(pluginWebc, {
+		components: "src/_components/**/*.webc",
+	});
 
-  eleventyConfig.setFrontMatterParsingOptions({
-    excerpt: true,
-    excerpt_separator: "<!-- excerpt -->",
-  });
+	eleventyConfig.setFrontMatterParsingOptions({
+		excerpt: true,
+		excerpt_separator: "<!-- excerpt -->",
+	});
 
-  eleventyConfig.addAsyncShortcode("image", imageShortcode);
+	eleventyConfig.addAsyncShortcode("image", imageShortcode);
 
-  eleventyConfig.setServerPassthroughCopyBehavior("copy");
-  eleventyConfig.addPassthroughCopy('src/assets/scss');
-  eleventyConfig.addPassthroughCopy('src/assets/javascript');
+	eleventyConfig.setServerPassthroughCopyBehavior("copy");
+	eleventyConfig.addPassthroughCopy("src/assets/scss");
+	eleventyConfig.addPassthroughCopy("src/assets/javascript");
 
-  return {
-    passthroughFileCopy: true,
-    dir: {
-      input: "src",
-      output: "site",
-    },
-  };
+	return {
+		passthroughFileCopy: true,
+		dir: {
+			input: "src",
+			output: "site",
+		},
+	};
 };
 
 async function imageShortcode(src, alt, sizes = "90vw") {
-  let metadata = await Image(src, {
-    widths: [400, 800],
-    formats: ["webp"],
-    urlPath: "/assets/images/",
-    outputDir: "./site/assets/images/",
-  });
+	let metadata = await Image(src, {
+		widths: [400, 800],
+		formats: ["webp"],
+		urlPath: "/assets/images/",
+		outputDir: "./site/assets/images/",
+	});
 
-  let imageAttributes = {
-    alt,
-    sizes,
-    class: 'image',
-    loading: "lazy",
-    decoding: "async",
-  };
+	let imageAttributes = {
+		alt,
+		sizes,
+		class: "image",
+		loading: "lazy",
+		decoding: "async",
+	};
 
-  return Image.generateHTML(metadata, imageAttributes);
+	return Image.generateHTML(metadata, imageAttributes);
 }
